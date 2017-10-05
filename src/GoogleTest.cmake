ac19e61586382a433425756509dc49b01643f3ad
Eric Dingle <eric.dingle@gmail.com>
2017-10-05 19:00:25 +0200

    Remove GoogleTest binary dir.

--- src/GoogleTest.cmake
+++ src/GoogleTest.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add(googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
   GIT_TAG           master
   SOURCE_DIR        "${CMAKE_CURRENT_SOURCE_DIR}/third_party/googletest"
-  BINARY_DIR        "${CMAKE_BINARY_DIR}/third_party/googletest/build"
+  BINARY_DIR        ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND     ""
   INSTALL_COMMAND   ""
