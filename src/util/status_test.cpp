297a8ddd0654ce7fb53d3241f640c69a7651aef9
Eric Dingle <eric.dingle@gmail.com>
2017-10-25 15:23:04 +0200

    Refactor status test.

--- src/util/status_test.cpp
+++ src/util/status_test.cpp
@@ -1,29 +1,44 @@
 #include "util/status.h"
 #include "third_party/googletest/googletest/include/gtest/gtest.h"
 
-TEST(StatusTest, Default) {
+class StatusTest : public testing::Test {
+ protected:
+  void ExpectStatus(const Status& status, const std::string& message, int line,
+                    int column) {
+    EXPECT_EQ(message, status.message());
+    EXPECT_EQ(line, status.line());
+    EXPECT_EQ(column, status.column());
+  }
+
+  void ExpectStatus(const Status& status, const std::string& message, int line,
+                    int column, bool error, const std::string& to_string) {
+    ExpectStatus(status, message, line, column);
+    EXPECT_EQ(error, status.error());
+    EXPECT_EQ(to_string, status.ToString());
+  }
+};
+
+TEST_F(StatusTest, DefaultConstructor) {
   Status status;
-  EXPECT_EQ("", status.message());
-  EXPECT_EQ(0, status.line());
-  EXPECT_EQ(0, status.column());
-  EXPECT_FALSE(status.error());
-  EXPECT_EQ("", status.ToString());
+  ExpectStatus(status, "", 0, 0, false, "");
 }
 
-TEST(StatusTest, ToString) {
+TEST_F(StatusTest, Constructor) {
   Status status("blah", 10, 20);
-  EXPECT_EQ("blah", status.message());
-  EXPECT_EQ(10, status.line());
-  EXPECT_EQ(20, status.column());
-  EXPECT_TRUE(status.error());
-  EXPECT_EQ("blah at line 10, column 20.", status.ToString());
+  ExpectStatus(status, "blah", 10, 20, true, "blah at line 10, column 20.");
 }
 
-TEST(StatusTest, ReturnIfError) {
+TEST_F(StatusTest, CopyConstructor) {
+  Status status1("a", 1, 1);
+  Status status2(status1);
+  ExpectStatus(status2, "a", 1, 1);
+}
+
+TEST_F(StatusTest, ReturnIfError) {
   auto func = []() -> Status {
     RETURN_IF_ERROR(Status());
     RETURN_IF_ERROR(Status("yes", 1, 1));
     return Status("no", 1, 1);
   };
-  EXPECT_EQ("yes", func().message());
+  ExpectStatus(func(), "yes", 1, 1);
 }
