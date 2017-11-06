2ffa5501978d509f4bce9f11d64790b9b535fe44
Eric Dingle <eric.dingle@gmail.com>
2017-11-05 20:30:20 -0500

    Remove redundant code from token stream test.

--- src/lexer/token_stream_test.cpp
+++ src/lexer/token_stream_test.cpp
@@ -23,32 +23,34 @@ class TestLexer : public Lexer {
   }
 };
 
-TEST(TokenStreamTest, GetNextToken) {
+class TokenStreamTest : public testing::Test {
+ protected:
+  Token GetNextToken(TokenStream* token_stream) {
+    return *token_stream->GetNextToken().value();
+  }
+};
+
+TEST_F(TokenStreamTest, GetNextToken) {
   TestLexer lexer;
   TokenStream token_stream(&lexer, "a bc\nde  ");
   EXPECT_TRUE(token_stream.HasInput());
 
-  EXPECT_TOKEN(*token_stream.GetNextToken().value(), TestLexer::TYPE_CHAR,
-               "a", 1, 1);
-  EXPECT_TOKEN(*token_stream.GetNextToken().value(), TestLexer::TYPE_CHAR,
-               "b", 1, 3);
-  EXPECT_TOKEN(*token_stream.GetNextToken().value(), TestLexer::TYPE_CHAR,
-               "c", 1, 4);
-  EXPECT_TOKEN(*token_stream.GetNextToken().value(), TestLexer::TYPE_CHAR,
-               "d", 2, 1);
-  EXPECT_TOKEN(*token_stream.GetNextToken().value(), TestLexer::TYPE_CHAR,
-               "e", 2, 2);
-  EXPECT_TOKEN(*token_stream.GetNextToken().value(),
-               TestLexer::TYPE_END_OF_INPUT, "(end of input)", 2, 5);
-  EXPECT_TOKEN(*token_stream.GetNextToken().value(),
-               TestLexer::TYPE_END_OF_INPUT, "(end of input)", 2, 5);
+  EXPECT_TOKEN(GetNextToken(&token_stream), TestLexer::TYPE_CHAR, "a", 1, 1);
+  EXPECT_TOKEN(GetNextToken(&token_stream), TestLexer::TYPE_CHAR, "b", 1, 3);
+  EXPECT_TOKEN(GetNextToken(&token_stream), TestLexer::TYPE_CHAR, "c", 1, 4);
+  EXPECT_TOKEN(GetNextToken(&token_stream), TestLexer::TYPE_CHAR, "d", 2, 1);
+  EXPECT_TOKEN(GetNextToken(&token_stream), TestLexer::TYPE_CHAR, "e", 2, 2);
+  EXPECT_TOKEN(GetNextToken(&token_stream), TestLexer::TYPE_END_OF_INPUT,
+               "(end of input)", 2, 5);
+  EXPECT_TOKEN(GetNextToken(&token_stream), TestLexer::TYPE_END_OF_INPUT,
+               "(end of input)", 2, 5);
 
   EXPECT_FALSE(token_stream.HasInput());
 }
 
-TEST(TokenStreamTest, GetNextTokenError) {
+TEST_F(TokenStreamTest, GetNextTokenError) {
   TestLexer lexer;
   TokenStream token_stream(&lexer, "f");
   EXPECT_TRUE(token_stream.HasInput());
-  EXPECT_EQ("fail", token_stream.GetNextToken().status().message());
+  EXPECT_STATUS(token_stream.GetNextToken().status(), "fail", 1, 1);
 }
