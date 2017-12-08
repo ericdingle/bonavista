8e1d77aefab0b0280cc2ab2619491b7c3695dfe2
Eric Dingle <eric.dingle@gmail.com>
2017-12-08 11:10:14 -0700

    Add missing parser test fixture.

---
+++ src/parser/parser_test_fixture.h
@@ -0,0 +1,21 @@
+#ifndef PARSER_PARSER_TEST_FIXTURE_H_
+#define PARSER_PARSER_TEST_FIXTURE_H_
+
+#include <memory>
+#include "lexer/token_stream.h"
+#include "parser/node.h"
+#include "third_party/googletest/googletest/include/gtest/gtest.h"
+#include "util/status_or.h"
+
+template <typename LexerT, typename ParserT>
+class ParserTestFixture : public testing::Test {
+ protected:
+  StatusOr<std::unique_ptr<Node>> Parse(const char* input) {
+    LexerT lexer;
+    TokenStream stream(&lexer, input);
+    ParserT parser(&stream);
+    return parser.Parse();
+  }
+};
+
+#endif  // PARSER_PARSER_TEST_FIXTURE_H_
